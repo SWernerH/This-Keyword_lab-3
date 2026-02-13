53ad91985db27d8a4ae970bd53c7f7a079db7b7b
WernerSosa <2024161143@ub.edu.bz>
2026-02-13 13:54:10 -0600

    Complete part 3

---
+++ exercise3.js
@@ -0,0 +1,23 @@
+const display = document.querySelector('#count-display');
+
+const CounterApp = {
+  count: 0,
+
+  increment: function() {
+    this.count++;
+    display.textContent = this.count;
+  },
+
+  decrement: function() {
+    this.count--;
+    display.textContent = this.count;
+  }
+};
+
+document
+  .querySelector('#inc-btn')
+  .addEventListener('click', CounterApp.increment.bind(CounterApp));
+
+document
+  .querySelector('#dec-btn')
+  .addEventListener('click', CounterApp.decrement.bind(CounterApp));
